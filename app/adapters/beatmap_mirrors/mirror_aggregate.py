771286ddf73165245970ca9a67f2a8b728cb47be
cmyui <cmyuiosu@gmail.com>
2024-06-22 12:44:04 -0400

    up to 20k for mino

--- app/adapters/beatmap_mirrors/mirror_aggregate.py
+++ app/adapters/beatmap_mirrors/mirror_aggregate.py
@@ -94,7 +94,11 @@ async def fetch_beatmap_zip_data(beatmapset_id: int) -> bytes | TimedOut | None:
             "beatmapset_id": beatmapset_id,
             "ms_elapsed": ms_elapsed,
             "data_size": len(result),
-            "bad_data": result if len(result) < 500 else None,
+            "bad_data": (
+                result
+                if len(result) < (20000 if mirror.name == "mino" else 500)
+                else None
+            ),
         },
     )
     return result
