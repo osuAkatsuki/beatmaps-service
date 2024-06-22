b329b360e6f60d2a0f8f4e32ba88734fafe7223c
cmyui <cmyuiosu@gmail.com>
2024-06-21 22:48:27 -0400

    Attach middleware

--- app/init_api.py
+++ app/init_api.py
@@ -1,13 +1,37 @@
+import logging
+
 from fastapi import FastAPI
+from fastapi import Request
+from fastapi import Response
+from starlette.middleware.base import RequestResponseEndpoint
 
 from app.api import api_router
 
 
+def init_routes(app: FastAPI) -> FastAPI:
+    app.include_router(api_router)
+    return app
+
+
+def init_middleware(app: FastAPI) -> FastAPI:
+    @app.middleware("http")
+    async def http_middleware(
+        request: Request,
+        call_next: RequestResponseEndpoint,
+    ) -> Response:
+        try:
+            return await call_next(request)
+        except BaseException:
+            logging.exception("Exception in ASGI application")
+            return Response(status_code=500)
+
+    return app
+
+
 def init_api() -> FastAPI:
     app = FastAPI()
-
-    app.include_router(api_router)
-
+    app = init_routes(app)
+    app = init_middleware(app)
     return app
 
 
