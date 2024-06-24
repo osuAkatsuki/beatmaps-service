e717f015eb66221483f174dbde60d8aaeca1b7f2
cmyui <cmyuiosu@gmail.com>
2024-06-24 02:53:45 -0400

    Add public endpoints

--- app/api/v1/cheesegull.py
+++ app/api/v1/cheesegull.py
@@ -126,6 +126,7 @@ class CheesegullBeatmapset(BaseModel):
 
 
 @router.get("/api/b/{beatmap_id}")
+@router.get("/api/public/b/{beatmap_id}")
 async def cheesegull_beatmap(
     beatmap_id: int,
     client_ip_address: str | None = Header(None, alias="X-Real-IP"),
@@ -148,6 +149,7 @@ async def cheesegull_beatmap(
 
 
 @router.get("/api/s/{beatmapset_id}")
+@router.get("/api/public/s/{beatmapset_id}")
 async def cheesegull_beatmapset(
     beatmapset_id: int,
     client_ip_address: str | None = Header(None, alias="X-Real-IP"),
@@ -189,6 +191,7 @@ def get_osu_api_v2_search_ranked_status(
 
 
 @router.get("/api/search")
+@router.get("/api/public/search")
 async def cheesegull_search(
     query: str = "",
     status: CheesegullRankedStatus | None = None,
