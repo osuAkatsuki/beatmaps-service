054a085be61b0c5ed46682cfe552f4e15ec6323e
cmyui <cmyuiosu@gmail.com>
2024-06-24 03:00:07 -0400

    The restTM

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
 from app.api.health import health_router
+from app.api.internal import internal_router
 from app.api.public import public_router
-from app.api.v1 import v1_router
 
 api_router = APIRouter()
 
-api_router.include_router(v1_router)
-api_router.include_router(public_router)
 api_router.include_router(health_router)
+api_router.include_router(internal_router)
+api_router.include_router(public_router)
--- app/api/public/__init__.py
+++ app/api/public/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from . import cheesegull
+from . import osz2_files
 
 public_router = APIRouter()
 
 public_router.include_router(cheesegull.router)
+public_router.include_router(osz2_files.router)
