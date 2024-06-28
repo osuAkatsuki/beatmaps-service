b40a93f5f2ff5fe65cb4e9011bf6639cc491477e
cmyui <cmyuiosu@gmail.com>
2024-06-28 18:27:09 -0400

    Update osu.direct url

--- app/adapters/osu_mirrors/backends/osu_direct.py
+++ app/adapters/osu_mirrors/backends/osu_direct.py
@@ -8,13 +8,13 @@ from app.adapters.osu_mirrors.backends import MirrorRequestError
 
 class OsuDirectMirror(AbstractBeatmapMirror):
     name = "osu_direct"
-    base_url = "https://api.osu.direct"
+    base_url = "https://osu.direct"
 
     async def fetch_beatmap_zip_data(self, beatmapset_id: int) -> bytes | None:
         try:
             logging.info(f"Fetching beatmapset osz2 from osu!direct: {beatmapset_id}")
             response = await self.http_client.get(
-                f"{self.base_url}/d/{beatmapset_id}",
+                f"{self.base_url}/api/d/{beatmapset_id}",
                 timeout=httpx.Timeout(None, connect=2),
             )
             if response.status_code == 404:
