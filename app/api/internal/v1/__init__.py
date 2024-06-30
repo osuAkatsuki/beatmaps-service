ea429a35de2cedb4611023cb8cc9bd0cc06e5a68
cmyui <cmyuiosu@gmail.com>
2024-06-30 06:17:20 -0400

    Minor ordering format change

--- app/api/internal/v1/__init__.py
+++ app/api/internal/v1/__init__.py
@@ -7,5 +7,5 @@ from . import osu_api_v2
 v1_router = APIRouter()
 
 v1_router.include_router(akatsuki.router)
-v1_router.include_router(osu_api_v2.router)
 v1_router.include_router(osu_api_v1.router)
+v1_router.include_router(osu_api_v2.router)
